2443ad420986aa1fe7a4026ab056f349f6a0dd2b
DjawadSev <98240427+DjawadSev@users.noreply.github.com>
2023-08-14 19:13:29 +0100

    logo change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Stack } from '@mui/material';
 
-import Logo from '../assets/images/logo.png';
+import Logo from '../assets/images/Logo-1.png;
 
 
 const Navbar = () => {
